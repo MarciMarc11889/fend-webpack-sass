d8f07d7495bcf077d08efad67e13d0ee5c0d7266
Marc pr <marc.rene@web.de>
2022-05-28 16:06:56 +0200

    implemented API code from website

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,4 +1,4 @@
-const CityUrl = 'https://api.openweathermap.org/data/2.5/weather?q=paris&appid=a448a4a826b1fca8eaa50dcd50dbc65d&units=metric' // Cityname &appid={API key}'
+const TestTextGerman = "Dies ist ein Testtext um zu sehen, dass die API richtig funktioniert."
 
 function handleSubmit(event) {
     event.preventDefault()
@@ -10,23 +10,41 @@ function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
 
-    // Define API Request to Open Weather Map 
-    const ApiRequest = async (CityUrl) => {
-        const res = await fetch(CityUrl)
-            try {
-                const data = res.json()
-                return data
-            }
-            catch(error) {
-                console.log("error", error);
-            }
-        }
+    const formdata = new FormData();
+    formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
+    formdata.append("txt", TestTextGerman);
+    
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+    };
+    
+    const response = fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
+    .then(response => ({
+        status: response.status, 
+        body: response.json()
+    }))
+    .then(status, body => console.log(status, body))
+    .catch(error => console.log('error', error));
 
-    ApiRequest(CityUrl)
-    .then (data => {
-        console.log(data);
-        document.getElementById('results').innerHTML = 'Current Temperature in '+ data.name + ' is ' + data.main.temp + ' °C'
-    })
+    // Define API Request to Open Weather Map 
+    // const ApiRequest = async (CityUrl) => {
+    //     const res = await fetch(CityUrl)
+    //         try {
+    //             const data = res.json()
+    //             return data
+    //         }
+    //         catch(error) {
+    //             console.log("error", error);
+    //         }
+    //     }
+
+    // ApiRequest(CityUrl)
+    // .then (data => {
+    //     console.log(data);
+    //     document.getElementById('results').innerHTML = 'Current Temperature in '+ data.name + ' is ' + data.main.temp + ' °C'
+    // })
 
 }
 
