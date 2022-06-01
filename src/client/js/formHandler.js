0be708caf0bf22ca749c81187499b04a768de98a
Wiediger <mwiedig@emea.corpdir.net>
2022-06-01 18:13:46 +0200

    inserted API request

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -14,27 +14,54 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::")
     
 // Request to API of Meaning Cloud. The API tells the User the Language of the entered Text
-    const formdata = new FormData();
-    formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
-    formdata.append("txt", TestTextGerman);
-    
-    const requestOptions = {
-        method: 'POST',
-        body: formdata,
-        redirect: 'follow'
-    };
-    
-    const response = fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
+const formdata = new FormData();
+formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
+formdata.append("txt", TestTextEnglish);
+formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
     .then(response => {
         const body = response.json()
         return body
     })
     .then((body) => {
-        console.log(body.language_list[0].name)
+        console.log(body)
+        
         return body
     }) 
-    .then((body) => document.getElementById('results').innerHTML = 'The entered Text is '+ body.language_list[0].name)
+    .then((body) => document.getElementById('results').innerHTML = 'The entered Text is '+ body)
     .catch(error => console.log('error', error));
+
+
+
+
+// const formdata = new FormData();
+    // formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
+    // formdata.append("txt", TestTextGerman);
+    
+    // const requestOptions = {
+    //     method: 'POST',
+    //     body: formdata,
+    //     redirect: 'follow'
+    // };
+    
+    // const response = fetch("https://api.meaningcloud.com/lang-4.0/identification", requestOptions)
+    // .then(response => {
+    //     const body = response.json()
+    //     return body
+    // })
+    // .then((body) => {
+    //     console.log(body.language_list[0].name)
+    //     return body
+    // }) 
+    // .then((body) => document.getElementById('results').innerHTML = 'The entered Text is '+ body.language_list[0].name)
+    // .catch(error => console.log('error', error));
 
 }
 
