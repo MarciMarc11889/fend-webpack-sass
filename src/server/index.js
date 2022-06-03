1290b4140ddd43c8b8b6eb321e4a6519e1c9a139
Marc Wiediger <marc.wiediger@mercedes-benz.com>
2022-06-04 00:48:37 +0200

    moved API request to server-side

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,15 @@ const mockAPIResponse = require('./mockAPI.js')
 var bodyParser = require('body-parser')
 var cors = require('cors')
 require('dotenv').config()
+const FormData = require('form-data')
+const fetch = require('node-fetch')
+const TestTextEnglish = "I'm afraid of coding"
+
+// To avoid an error with ssl-certificate
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+let appData ={}
+
 
 var json = {
     'title': 'test json response',
@@ -24,44 +33,44 @@ app.use(express.static('dist'))
 
 console.log(JSON.stringify(mockAPIResponse))
 
-app.post('/api', (req, res) => {
-    console.log('I got a request')
-    const respond = {
-        name:'Marc',
-        age: '32',
-        favourite_meal: 'Spaghetti'
-    }
-    res.send(respond)
+
+
+const getInformation = async (appData) => {
+// Request to API of Meaning Cloud. 
+const formdata = new FormData();
+formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
+formdata.append("txt", TestTextEnglish);
+formdata.append("lang", "en");  // 2-letter code, like en es fr ...
+
+const requestOptions = {
+  method: 'POST',
+  body: formdata,
+  redirect: 'follow'
+};
+
+const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+    .then(response => {
+        const body = response.json()
+        return body
+    })
+    .then(body => {
+        console.log(body)
+        const polarity=body.score_tag
+        const subjectivity =body.subjectivity
+        const text = body.sentence_list[0].text
+        appData ={polarity, subjectivity, text}
+        return appData
+    })
+    .catch(error => console.log('error', error));
+
+    return appData
+}
+
+app.post('/api', (req, res, appData) => {
+    getInformation()
+    console.log(appData)
+    res.send(appData)
   })
-
-// Request to API of Meaning Cloud. The API tells the User the Language of the entered Text
-// const formdata = new FormData();
-// formdata.append("key", "bd57c24533f8c58c206c97734b056bb6");
-// formdata.append("txt", TestTextEnglish);
-// formdata.append("lang", "en");  // 2-letter code, like en es fr ...
-
-// const requestOptions = {
-//   method: 'POST',
-//   body: formdata,
-//   redirect: 'follow'
-// };
-
-// const response = fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-//     .then(response => {
-//         const body = response.json()
-//         return body
-//     })
-//     .then((body) => {
-//         console.log(body)
-//         const polarity=body.score_tag
-//         const subjectivity =body.subjectivity
-//         const text = body.sentence_list[0].text
-//         return {polarity, subjectivity, text}
-//     }) 
-//     .then(({polarity, subjectivity, text}) => 
-//         document.getElementById('results').innerHTML = 'Polarity: '+ polarity + '<br>Subjectivity: '+ subjectivity + '<br>Text: ' + text)
-//     .catch(error => console.log('error', error));
-
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
