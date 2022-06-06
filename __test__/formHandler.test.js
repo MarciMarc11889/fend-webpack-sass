b2e5238e0c335eac0c3c3ab7451b551463ff9af3
Marc Wiediger <marc.wiediger@mercedes-benz.com>
2022-06-06 13:11:32 +0200

    Switched to 'easy' Tests from Udacity

--- __test__/formHandler.test.js
+++ __test__/formHandler.test.js
@@ -1,20 +1,7 @@
-// const apiRequest = require("../src/client/js/formHandler")
-import {apiRequest} from "../src/client/js/formHandler"
+import {handleSubmit} from "../src/client/js/formHandler"
 
-describe('Testing the submit functionality', () => {
-    test('Polarity schould be NONE', ()=>{
-        // expect.assertions(1);
-        const formText = {submittedText:"Hi, my name is Frederik."};
-        const output={
-            polarity:'NONE',
-            subjectivity: 'OBJECTIVE',
-            text: 'Hi, my name is Frederik.'
-        };
 
-        return apiRequest(formText).then (data => {
-            expect(data).toEqual(output)
-        })
-        
-
-    })
-  });
+describe("Testing the submit functionality", () => {
+    test("Testing the handleSubmit() function", () => {
+        expect(handleSubmit).toBeDefined();
+    })});
